966d85c4c4d8e22c9cf1e534e8b9e94cdd5d745c
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-06-27 00:57:12 +0300

    Lists_01 added

---
+++ src/June2021/Lists_01.java
@@ -0,0 +1,21 @@
+package June2021;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Lists_01 {
+    public static Object createArrayList() {
+        ArrayList<String> list = new ArrayList<>();
+        return list;
+
+    }
+
+    public static Object createLinkedList() {
+        LinkedList<String> list = new LinkedList<>();
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
