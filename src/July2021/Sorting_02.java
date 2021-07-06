f194d48f8ec0edd431f943eeab8742f62dbb7e4f
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-06 15:09:20 +0300

    Sorting_02 added

---
+++ src/July2021/Sorting_02.java
@@ -0,0 +1,40 @@
+package July2021;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sorting_02 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        String a;
+        String b;
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (isGreaterThan(array[i], array[i + 1])) {
+                    a = array[i];
+                    b = array[i + 1];
+                    array [i] = b;
+                    array[i + 1] = a;
+                }
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
