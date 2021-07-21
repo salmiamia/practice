e345220310c0803bbc62eed34194d0aab82366a6
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-21 22:09:26 +0300

    TypeConversion_05

---
+++ src/July2021/TypeConversion_05.java
@@ -0,0 +1,13 @@
+package July2021;
+
+public class TypeConversion_05 {
+    public static void main(String[] args) {
+        double d = (short) 2.50256e2d;
+        char c = (short) 'd';
+        short s = (short) 2.22;
+        int i = 150000;
+        float f = 0.50f;
+        double result = f + (i / c) - (d * s) - 500e-3;
+        System.out.println("result: " + result);
+    }
+}
