c456e41fa61c5b7cf269ac0288f8db8293130326
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-22 23:56:44 +0300

    Sorting_04

---
+++ src/July2021/Sorting_04.java
@@ -0,0 +1,24 @@
+package July2021;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sorting_04 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[9]);
+        System.out.println(array[10]);
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+}
