9135d0d1bb5a1ca144ac7fa089da1b92689f32e6
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-22 23:06:15 +0300

    TypeConversion_10

---
+++ src/July2021/TypeConversion_10.java
@@ -0,0 +1,13 @@
+package July2021;
+
+public class TypeConversion_10 {
+    public static void main(String[] args) {
+        int a = 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = (byte) a + b / c / e;
+
+        System.out.println(d);
+    }
+}
