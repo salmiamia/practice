1e0d6dd6fef6b5d389203ab6ea028dd099db7d58
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-25 23:50:08 +0300

    Inheritance_03

---
+++ src/July2021/Inheritance_03.java
@@ -0,0 +1,30 @@
+package July2021;
+
+public class Inheritance_03 {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Sheep extends Animal {
+
+    }
+
+    public class Bull extends Animal {
+
+    }
+
+    public class Goat extends Animal {
+
+    }
+}
