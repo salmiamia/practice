db6faa16498a039071d530c3c48d71f17c3f6ebe
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 00:00:03 +0300

    Inheritance_04

---
+++ src/July2021/Inheritance_04.java
@@ -0,0 +1,22 @@
+package July2021;
+
+public class Inheritance_04 {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
