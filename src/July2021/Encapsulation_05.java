e0d23992b7ace1a3abb4e22a4f8cf5434bd187a0
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 12:27:03 +0300

    Encapsulation_05

---
+++ src/July2021/Encapsulation_05.java
@@ -0,0 +1,34 @@
+package July2021;
+
+public class Encapsulation_05 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
