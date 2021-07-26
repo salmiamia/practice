467886c738aeceb0277a84a70193f9ac16d15987
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 12:07:38 +0300

    Encapsulation_01

---
+++ src/July2021/Encapsulation_01.java
@@ -0,0 +1,21 @@
+package July2021;
+
+public class Encapsulation_01 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat() {
+        }
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
+}
