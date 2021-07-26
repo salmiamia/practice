a709dc387f277b95e5deaa7d39199a9c1890dcd7
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 22:14:05 +0300

    Encapsulation_07

---
+++ src/July2021/Encapsulation_07.java
@@ -0,0 +1,14 @@
+package July2021;
+
+public class Encapsulation_07 {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple {
+
+    }
+}
