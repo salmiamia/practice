dd081329994fe245695766b7eee0459bf1c4ec26
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 22:24:37 +0300

    Encapsulation_09

---
+++ src/July2021/Encapsulation_09.java
@@ -0,0 +1,18 @@
+package July2021;
+
+public class Encapsulation_09 {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish {
+
+    }
+
+    public class Dinosaur extends Lizard {
+
+    }
+}
