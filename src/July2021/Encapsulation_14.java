3db329fd8aa3557c5a839365ac63378a6fbc4376
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 22:38:19 +0300

    Encapsulation_14

---
+++ src/July2021/Encapsulation_14.java
@@ -0,0 +1,22 @@
+package July2021;
+
+public class Encapsulation_14 {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
