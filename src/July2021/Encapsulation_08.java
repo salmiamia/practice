239d94fb965e7a33e8257e03b8ba263065f65b29
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 22:23:30 +0300

    Encapsulation_08

---
+++ src/July2021/Encapsulation_08.java
@@ -0,0 +1,22 @@
+package July2021;
+
+public class Encapsulation_08 {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+}
