aebfff792ea5a4750ac72aac9a5d3da60e17854e
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-26 22:29:04 +0300

    Encapsulation_11

---
+++ src/July2021/Encapsulation_11.java
@@ -0,0 +1,18 @@
+package July2021;
+
+public class Encapsulation_11 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
