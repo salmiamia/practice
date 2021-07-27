f0833893703cd60f12f7a0bf8fe89e3fc5b57c22
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-27 23:27:30 +0300

    Overload_05

---
+++ src/July2021/Overload_05.java
@@ -0,0 +1,18 @@
+package July2021;
+
+public class Overload_05 {
+    public static void main(String[] args) {
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
+
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+}
