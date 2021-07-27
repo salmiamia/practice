ff130c06417805564c09e6d80a2c7e158424df8a
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-27 22:54:41 +0300

    Overload_01

---
+++ src/July2021/Overload_01.java
@@ -0,0 +1,13 @@
+package July2021;
+
+public class Overload_01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+    }
+
+    public static void print(String a) {
+    }
+}
