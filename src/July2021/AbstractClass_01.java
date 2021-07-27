7094224c0def3e14eb5259ca558f184af0782b0a
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-28 00:47:07 +0300

    AbstractClass_01

---
+++ src/July2021/AbstractClass_01.java
@@ -0,0 +1,13 @@
+package July2021;
+
+public class AbstractClass_01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+}
