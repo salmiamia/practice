845f88142b7c4beb1401af86b721a71d0a0c75dd
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-27 23:03:51 +0300

    Overload_02

---
+++ src/July2021/Overload_02.java
@@ -0,0 +1,14 @@
+package July2021;
+
+public class Overload_02 {
+    public static void main(String[] args) {
+        print(5);
+        print((Integer) 5);
+    }
+
+    public static void print(int a){
+    }
+
+    public static void print(Integer a){
+    }
+}
