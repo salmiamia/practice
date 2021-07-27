1eac0c637338c262c654867fd4296981c483490f
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-27 23:05:56 +0300

    Overload_03

---
+++ src/July2021/Overload_03.java
@@ -0,0 +1,22 @@
+package July2021;
+
+public class Overload_03 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+    }
+
+    public static void print(String s) {
+    }
+
+    public static void print(boolean b) {
+    }
+
+    public static void print(long l) {
+    }
+
+    public static void print(char ch) {
+    }
+}
