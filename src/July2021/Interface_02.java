35a0f3feb3ad32d97e37fe8938b1b14f1868385d
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-29 12:19:04 +0300

    Interface_02

---
+++ src/July2021/Interface_02.java
@@ -0,0 +1,19 @@
+package July2021;
+
+public class Interface_02 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanRun{
+        String a();
+    }
+
+    public interface CanFly{
+        String a();
+    }
+
+    public interface CanSwim{
+        String a();
+    }
+}
