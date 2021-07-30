04eacb19168bd22feecb56a18e6f8c7639ecfd92
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-30 16:18:34 +0300

    Interface_03

---
+++ src/July2021/Interface_03.java
@@ -0,0 +1,34 @@
+package July2021;
+
+public class Interface_03 {
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human);
+    }
+
+    public static interface Worker {
+        public void workLazy();
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static interface Secretary {
+        public void workLazy();
+    }
+
+    public static interface Miner {
+        public void workVeryHard();
+    }
+
+    public static class Human implements Businessman, Worker, Secretary {
+
+        public void workHard() {
+        }
+
+        public void workLazy() {
+        }
+
+    }
+}
