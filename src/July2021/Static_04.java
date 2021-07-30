f6a0616ddbb085f957dba687610b920798efd59e
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-31 01:07:54 +0300

    Static_04

---
+++ src/July2021/Static_04.java
@@ -0,0 +1,27 @@
+package July2021;
+
+public class Static_04 {
+    public static void main(String[] args) {
+
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        static Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby implements Dream, Desire {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+}
