90bb03a84f068b066b01e2e803ae5c3d0a347f4e
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-07-31 02:48:20 +0300

    Inheritance_23

---
+++ src/July2021/Inheritance_23.java
@@ -0,0 +1,33 @@
+package July2021;
+
+import java.awt.*;
+
+public class Inheritance_23 {
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+
+    public static class BigFox extends Fox {
+        public Color Color() {
+            return Color.GRAY;
+        }
+
+        @Override
+        public Color getColor() {
+            return null;
+        }
+    }
+}
