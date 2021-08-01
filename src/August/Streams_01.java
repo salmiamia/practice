5181552aa67dd40afd4786cc31b92ca4ed33edc4
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-08-01 22:45:13 +0300

    Streams_01

---
+++ src/August/Streams_01.java
@@ -0,0 +1,37 @@
+package August;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Streams_01 {
+    public static class BufferedInputExample {
+
+        public static void main(String[] args) throws Exception {
+            InputStream inputStream = null;
+            BufferedInputStream buffer = null;
+
+            try {
+
+                inputStream = new FileInputStream("C:/java/practice/src/August/example.txt");
+
+                buffer = new BufferedInputStream(inputStream);
+
+                while(buffer.available()>0) {
+
+                    char c = (char)buffer.read();
+
+                    System.out.println("Был прочитан символ " + c);
+                }
+            } catch(Exception e) {
+
+                e.printStackTrace();
+
+            } finally {
+
+                inputStream.close();
+                buffer.close();
+            }
+        }
+    }
+}
