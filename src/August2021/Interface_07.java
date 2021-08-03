81879e49a063774a807173841560ddad06c237bd
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-08-03 23:03:28 +0300

    Interface_07

---
+++ src/August2021/Interface_07.java
@@ -0,0 +1,29 @@
+package August2021;
+
+import java.awt.*;
+
+public class Interface_07 {
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+
+    public static class BigFox extends Fox {
+
+        @Override
+        public Color getColor() {
+            return null;
+        }
+    }
+}
