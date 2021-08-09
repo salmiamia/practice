0c11195470ee2a123f80a24f1cab3171721286c3
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-08-09 21:28:27 +0300

    RepkaStory

---
+++ src/August2021/RepkaStory/task1327/RepkaItem.java
@@ -0,0 +1,5 @@
+package August2021.RepkaStory.task1327;
+
+public interface RepkaItem {
+    public String getNamePadezh();
+}
---
+++ src/August2021/RepkaStory/task1327/RepkaStory.java
@@ -0,0 +1,15 @@
+package August2021.RepkaStory.task1327;
+
+import java.util.List;
+
+public class RepkaStory {
+    static void tell(List<Person> items) {
+        Person first;
+        Person second;
+        for (int i = items.size() - 1; i > 0; i--) {
+            first = items.get(i);
+            second = items.get(i - 1);
+            first.pull(second);
+        }
+    }
+}
