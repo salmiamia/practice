2a00fc1400f7e41e73a9ecdd1f6b8eb814f77901
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-08-24 22:00:29 +0300

    Bridge

---
+++ src/August2021/Bridge/SuspensionBridge.java
@@ -0,0 +1,9 @@
+package August2021.Bridge;
+
+public class SuspensionBridge implements com.javarush.task.task14.task1409.Bridge {
+
+    @Override
+    public int getCarsCount() {
+        return 5;
+    }
+}
