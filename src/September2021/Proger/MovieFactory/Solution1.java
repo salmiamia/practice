661f1c846a994a8f1feeb9254bd9f4b80bb8067a
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-09-07 22:31:28 +0300

    MovieFactory

---
+++ src/September2021/Proger/MovieFactory/Solution1.java
@@ -0,0 +1,53 @@
+package September2021.Proger.MovieFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+
+        while (string.equals("soapOpera") || string.equals("cartoon") || string.equals("thriller")) {
+            Movie movie = MovieFactory.getMovie(string);
+            System.out.println(movie.getClass().getSimpleName());
+            string = reader.readLine();
+        }
+        Movie movie = MovieFactory.getMovie(string);
+
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            }
+
+            if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+
+            if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie{
+    }
+}
