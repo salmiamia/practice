6b7391d00ef3383c12335523c3b7a1ddfe843225
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-09-25 15:14:11 +0300

    Inheritance_04

---
+++ src/September2021/Inheritance_04.java
@@ -0,0 +1,18 @@
+package September2021;
+
+public class Inheritance_04 {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+
+    }
+
+    public class Car extends Machine{
+
+    }
+}
