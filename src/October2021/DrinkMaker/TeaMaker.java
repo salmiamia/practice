2473d8794a80d03d5ba9ca3ccdb1f13a4b0af14f
Liudmila Ermakova <l.eroshkina@gmail.com>
2021-10-20 00:38:06 +0300

    DrinkMaker

---
+++ src/October2021/DrinkMaker/TeaMaker.java
@@ -0,0 +1,18 @@
+package October2021.DrinkMaker;
+
+public class TeaMaker extends DrinkMaker {
+    @Override
+    void getRightCup() {
+        System.out.println("Берем чашку для чая");
+    }
+
+    @Override
+    void putIngredient() {
+        System.out.println("Насыпаем чай");
+    }
+
+    @Override
+    void pour() {
+        System.out.println("Заливаем кипятком");
+    }
+}
