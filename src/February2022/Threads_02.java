d451d9122b4f15fded807e30137c71b2e8a099be
Liudmila Ermakova <l.eroshkina@gmail.com>
2022-02-17 23:33:21 +0300

    Threads_02

---
+++ src/February2022/Threads_02.java
@@ -0,0 +1,29 @@
+package February2022;
+
+public class Threads_02 {
+    public static void main(String[] args) throws InterruptedException {
+        Mouse alpha = new Mouse("#1");
+        Mouse mouse1 = new Mouse("#2");
+        Mouse mouse2 = new Mouse("#3");
+    }
+
+    private static void eat() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static class Mouse extends Thread {
+        public Mouse(String name) {
+            super(name);
+            start();
+        }
+
+        public void run() {
+            System.out.println(getName() + " is starting to eat");
+            eat();
+            System.out.println(getName() + " has finished eating");
+        }
+    }
+}
