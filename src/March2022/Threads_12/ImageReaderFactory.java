b532680bbb462a6d78fccc7de9705349a0ec5801
Liudmila Ermakova <l.eroshkina@gmail.com>
2022-04-18 23:18:46 +0300

    Threads_12

--- src/March2022/Threads_12/ImageReaderFactory.java
+++ src/March2022/Threads_12/ImageReaderFactory.java
@@ -1,13 +1,12 @@
 package March2022.Threads_12;
 
-import com.javarush.task.task16.task1631.common.*;
 
 public class ImageReaderFactory {
 
-    public static com.javarush.task.task16.task1631.common.ImageReader getImageReader(com.javarush.task.task16.task1631.common.ImageTypes a) {
-        if(a == com.javarush.task.task16.task1631.common.ImageTypes.JPG) return new com.javarush.task.task16.task1631.common.JpgReader();
-        else if(a == com.javarush.task.task16.task1631.common.ImageTypes.BMP) return new com.javarush.task.task16.task1631.common.BmpReader();
-        else if(a == com.javarush.task.task16.task1631.common.ImageTypes.PNG) return new com.javarush.task.task16.task1631.common.PngReader();
+    public static ImageReader getImageReader(ImageTypes a) {
+        if(a == ImageTypes.JPG) return new JpgReader();
+        else if(a == ImageTypes.BMP) return new BmpReader();
+        else if(a == ImageTypes.PNG) return new PngReader();
         else throw new IllegalArgumentException("Неизвестный тип картинки");
     }
 }
